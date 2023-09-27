306ecdc70c53d094a287163fb49c64222ba6e0cd
Dean Chapman <dean.chapman@kroo.com>
2023-09-27 20:09:16 +0100

    Add delay to test

--- projects/basic-async/test/basic_async_test.clj
+++ projects/basic-async/test/basic_async_test.clj
@@ -6,5 +6,6 @@
   (sut/with-system
     (fn [state]
       ((-> state :command-sender) {:type :InviteCustomer :email "dean@p14n.com"})
+      (Thread/sleep 100)
       (is (= true
              (-> state :db deref vals first :invited))))))
